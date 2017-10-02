e19c8808877ad14501693bb79915dd4fc71c9d55
Bar-Shay <bbshay4@gmail.com>
2017-10-02 17:42:54 +0300

    test

--- app/src/main/java/com/example/madaim/ex1/MainActivity.java
+++ app/src/main/java/com/example/madaim/ex1/MainActivity.java
@@ -9,5 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //I got 1 2 3 4 5 6 7 8 m's in bank account
     }
 }
